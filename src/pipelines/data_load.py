af9fb15e2b8637d709cca3c5f27409f29352c359
João Santos <jfasantos@gmail.com>
2021-01-26 13:52:48 +0000

    Add Data Load Pipeline: update data_load.py to use main function

--- src/pipelines/data_load.py
+++ src/pipelines/data_load.py
@@ -1,5 +1,6 @@
 from typing import Text
 import yaml
+import argparse
 
 from src.data.dataset import get_dataset
 
@@ -13,3 +14,11 @@ def data_load(config_path: Text) -> None:
     config = yaml.safe_load(open(config_path))
     dataset = get_dataset()
     dataset.to_csv(config['data_load']['dataset_csv'], index=False)
+
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--config", dest='config', required=True)
+    args = args_parser.parse_args()
+    
+    data_load(config_path=args.config)
